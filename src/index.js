deacc360c3e2445dfd12061885e2347912d625df
xWz5 <xwzl509@gmail.com>
2026-03-30 14:50:19 +0300

    update title

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
